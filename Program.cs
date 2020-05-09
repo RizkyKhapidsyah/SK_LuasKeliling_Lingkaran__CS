c959fded766a2f88ecb69c639b110ec6b3ffb7c5
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-05-09 16:44:02 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SK_LuasKeliling_Lingkaran__CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.14;
+            string JariJari;
+            double Konversi, Luas, Keliling;
+
+            Console.Write("Masukkan Jari Jari  : ");
+            JariJari = Console.ReadLine();
+
+            Konversi = Convert.ToDouble(JariJari);
+
+            Luas = PI * Konversi * Konversi;
+            Keliling = 2 * PI * Konversi;
+
+            Console.WriteLine("\nLuas Lingkaran      = {0}", Luas);
+            Console.WriteLine("Keliling Lingkaran  = {0}", Keliling);
+            Console.Read();
+        }
+    }
+}
